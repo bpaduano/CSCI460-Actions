e964b3f2d9d0b77de6a6fa9ad7b298de5b43982e
Brendon Paduano <99129496+bpaduano@users.noreply.github.com>
2024-06-10 16:28:45 -0400

    Update Main.java

--- Main.java
+++ Main.java
@@ -1,5 +1,7 @@
-public class Main {
-    public static void main(String[] args) {
-        System.out.printl("Hello World!");
-    }
-}
+def hello():
+  print("Hi")
+
+def bye():
+	print("bye")
+
+print(hello())
